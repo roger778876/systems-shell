3cf3db14d76991accd86ab2751a913100bff01ae
Cesar <rogercesar778876@gmail.com>
2017-11-30 11:45:35 -0500

    input.c

--- input.c
+++ input.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 
-char **parse_args() {
-  char input[256] = "hello this is";
-  char *line = input; 
-  // fgets(line, 256, stdin);
-  char **arguments = malloc(20 * sizeof(char *));
+char **parse_args(char *line) {
+  int counter = 0;
+  char line2[100];
+  strcpy(line2, line);
+  while (line){
+    strsep(&line, " ");
+    counter++;
+  }
+  printf("%d\n", counter);
+  char **arguments = malloc(counter * sizeof(char *));
   int i = 0;
-  while(line) {
-    arguments[i] = strsep(&line, " ");
+  char *line3 = line2;
+  printf("line3: %s\n", line3 + 4);
+  while(line3) {
+    arguments[i] = strsep(&line3, " ");
     i++;
   }
   return arguments;
 }
 
 int main() {
-  char **args = parse_args();
-  printf("%s\n", args[1]);
+  char *line;
+  fgets(line, 256, stdin);
+  char **args = parse_args(line);
+  printf("Running '%s'\n", line);
+  printf("[%s]\n", args[0]);
 
   return 0;
 }
